4a25f3af9d0b3290521e98c21244920bb670aa66
Robert O'Shea <robertosheagamedev@gmail.com>
2026-03-23 02:23:16 +0000

    Add Stardiver WS set

--- DRG/WS.lua
+++ DRG/WS.lua
@@ -1,6 +1,14 @@
 return {
-  ammo = "Knobkierrie",
-  feet = "Sulev. Leggings +1",
-  right_ear = "Thrud Earring",
-  right_ring = "Karieyh Ring",
+  default = {
+    ammo = "Knobkierrie",
+    feet = "Sulev. Leggings +1",
+    right_ear = "Thrud Earring",
+    right_ring = "Karieyh Ring",
+  },
+  ["Stardiver"] = {
+    waist = "Fotia Belt",
+    left_ring = "Hetairoi Ring",
+    left_ear = "Brutal Earring",
+    right_ear = "Moonshade Earring",
+  },
 }
