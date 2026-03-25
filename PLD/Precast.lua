d4ce5b006b20d117a9d5835eb8e71ef55d1de3ab
Robert O'Shea <robertosheagamedev@gmail.com>
2026-03-25 23:30:41 +0000

    Increase Fast Cast for precast

--- PLD/Precast.lua
+++ PLD/Precast.lua
@@ -1,16 +1,17 @@
 return {
   default = {
+    ammo = "Ginsen",
     head = "Chev. Armet +1",
-    body = { name = "Souveran Cuirass", augments = { 'HP+80', 'Enmity+7', 'Potency of "Cure" effect received +10%', } },
+    body = "Rev. Surcoat +3",
     hands = "Sulev. Gauntlets +1",
-    legs = "Chev. Cuisses +2",
-    feet = { name = "Eschite Greaves", augments = { 'HP+80', 'Enmity+7', 'Phys. dmg. taken -4', } },
+    legs = "Enif Cosciales",
+    feet = { name = "Carmine Greaves", augments = { 'HP+60', 'MP+60', 'Phys. dmg. taken -3', } },
     neck = "Voltsurge Torque",
     waist = "Creed Baudrier",
     left_ear = "Loquac. Earring",
     right_ear = "Friomisi Earring",
-    left_ring = "Gelatinous Ring",
-    right_ring = "Sulevia's Ring",
+    left_ring = "Naji's Loop",
+    right_ring = "Kishar Ring",
     back = "Rudianos's Mantle",
   },
   spells = {
