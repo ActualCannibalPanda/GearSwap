a6f2fad1dca72650188387e0df0e79083f0a91bc
Robert O'Shea <robertosheagamedev@gmail.com>
2026-03-26 11:27:40 +0000

    Update DRK

--- DRK/TP.lua
+++ DRK/TP.lua
@@ -2,9 +2,9 @@ return {
   default = {
     ammo = 'Ginsen',
     head = 'Flam. Zucchetto +2',
-    body = "Sulevia's Plate. +1",
-    hands = 'Flam. Manopolas +1',
-    legs = 'Sulevi. Cuisses +1',
+    body = "Sakpata's Plate",
+    hands = "Sakpata's Gauntlets",
+    legs = "Sakpata's Cuisses",
     feet = 'Flam. Gambieras +2',
     neck = 'Sanctity Necklace',
     waist = 'Ioskeha Belt',
