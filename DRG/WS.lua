4ccd9cacfe1ac9edddaaae11945fb954e7f0b5b1
Robert O'Shea <robertosheagamedev@gmail.com>
2026-03-28 14:18:04 +0000

    Formatting

--- DRG/WS.lua
+++ DRG/WS.lua
@@ -1,14 +1,14 @@
 return {
   default = {
-    ammo = "Knobkierrie",
-    feet = "Sulev. Leggings +1",
-    right_ear = "Thrud Earring",
-    right_ring = "Karieyh Ring",
+    ammo = 'Knobkierrie',
+    feet = 'Sulev. Leggings +1',
+    right_ear = 'Thrud Earring',
+    right_ring = 'Karieyh Ring',
   },
-  ["Stardiver"] = {
-    waist = "Fotia Belt",
-    left_ring = "Hetairoi Ring",
-    left_ear = "Brutal Earring",
-    right_ear = "Moonshade Earring",
+  ['Stardiver'] = {
+    waist = 'Fotia Belt',
+    left_ring = 'Hetairoi Ring',
+    left_ear = 'Brutal Earring',
+    right_ear = 'Moonshade Earring',
   },
 }
