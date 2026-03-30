0b2e11e2c0d760c0154ec2e305c9bfcc788c6cf9
Robert O'Shea <robertosheagamedev@gmail.com>
2026-03-30 15:17:03 +0100

    Add to WS set

--- DRG/WS.lua
+++ DRG/WS.lua
@@ -6,9 +6,16 @@ return {
     right_ring = 'Karieyh Ring',
   },
   ['Stardiver'] = {
+    head = 'Flam. Zucchetto +2',
+    body = 'Nyame Mail',
+    hands = 'Sulev. Gauntlets +1',
+    legs = 'Sulevi. Cuisses +1',
+    feet = 'Sulev. Leggings +2',
+    neck = 'Fotia Gorget',
     waist = 'Fotia Belt',
-    left_ring = 'Hetairoi Ring',
     left_ear = 'Brutal Earring',
     right_ear = 'Moonshade Earring',
+    left_ring = 'Hetairoi Ring',
+    right_ring = 'Karieyh Ring',
   },
 }
