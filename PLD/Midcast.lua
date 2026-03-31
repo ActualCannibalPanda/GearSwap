1a096a2c9f8ad399ddfc800502a18d984798a85a
Robert O'Shea <robertosheagamedev@gmail.com>
2026-03-31 02:30:02 +0100

    Adjust magic emnity sets

--- PLD/Midcast.lua
+++ PLD/Midcast.lua
@@ -1,28 +1,18 @@
 return {
   ['Healing Magic'] = {
     default = {
-      ammo = 'Ginsen',
-      head = {
-        name = 'Souveran Schaller',
-        augments = { 'HP+80', 'Enmity+7', 'Potency of "Cure" effect received +10%' },
+      ammo = 'Staunch Tathlum',
+      head = { name = 'Souveran Schaller', augments = { 'HP+80', 'Enmity+7', 'Potency of "Cure" effect received +10%' } },
+      body = 'Chev. Cuirass +2',
+      hands = { name = 'Souv. Handsch. +1', augments = { 'HP+105', 'Enmity+9', 'Potency of "Cure" effect received +15%' } },
+      legs = { name = 'Carmine Cuisses +1', augments = { 'Accuracy+20', 'Attack+12', '"Dual Wield"+6' } },
+      feet = {
+        name = 'Odyssean Greaves',
+        augments = { 'Blood Pact Dmg.+1', '"Fast Cast"+3', 'Phalanx +4', 'Mag. Acc.+4 "Mag.Atk.Bns."+4' },
       },
-      body = { name = 'Souveran Cuirass', augments = { 'HP+80', 'Enmity+7', 'Potency of "Cure" effect received +10%' } },
-      hands = {
-        name = 'Souv. Handsch. +1',
-        augments = { 'HP+105', 'Enmity+9', 'Potency of "Cure" effect received +15%' },
-      },
-      legs = {
-        name = 'Souv. Diechlings +1',
-        augments = {
-          'HP+105',
-          'Enmity+9',
-          'Potency of "Cure" effect received +15%',
-        },
-      },
-      feet = 'Souveran Schuhs',
       neck = 'Unmoving Collar',
-      waist = 'Creed Baudrier',
-      left_ear = 'Friomisi Earring',
+      waist = 'Rumination Sash',
+      left_ear = 'Halasz Earring',
       right_ear = 'Cryptic Earring',
       left_ring = 'Apeile Ring',
       right_ring = 'Supershear Ring',
@@ -32,24 +22,24 @@ return {
   },
   ['Enhancing Magic'] = {
     default = {
-      ammo = 'Ginsen',
+      ammo = 'Staunch Tathlum',
       head = {
         name = 'Souveran Schaller',
         augments = { 'HP+80', 'Enmity+7', 'Potency of "Cure" effect received +10%' },
       },
-      body = { name = 'Souveran Cuirass', augments = { 'HP+80', 'Enmity+7', 'Potency of "Cure" effect received +10%' } },
+      body = 'Chev. Cuirass +2',
       hands = {
         name = 'Souv. Handsch. +1',
         augments = { 'HP+105', 'Enmity+9', 'Potency of "Cure" effect received +15%' },
       },
-      legs = {
-        name = 'Souv. Diechlings +1',
-        augments = { 'HP+105', 'Enmity+9', 'Potency of "Cure" effect received +15%' },
+      legs = { name = 'Carmine Cuisses +1', augments = { 'Accuracy+20', 'Attack+12', '"Dual Wield"+6' } },
+      feet = {
+        name = 'Odyssean Greaves',
+        augments = { 'Blood Pact Dmg.+1', '"Fast Cast"+3', 'Phalanx +4', 'Mag. Acc.+4 "Mag.Atk.Bns."+4' },
       },
-      feet = 'Souveran Schuhs',
       neck = 'Unmoving Collar',
-      waist = 'Creed Baudrier',
-      left_ear = 'Friomisi Earring',
+      waist = 'Rumination Sash',
+      left_ear = 'Halasz Earring',
       right_ear = 'Cryptic Earring',
       left_ring = 'Apeile Ring',
       right_ring = 'Supershear Ring',
@@ -58,7 +48,10 @@ return {
     barspells = {},
     spells = {
       ['Flash'] = {
-        feet = { name = 'Carmine Greaves', augments = { 'HP+60', 'MP+60', 'Phys. dmg. taken -3' } },
+        feet = {
+          name = 'Odyssean Greaves',
+          augments = { 'Blood Pact Dmg.+1', '"Fast Cast"+3', 'Phalanx +4', 'Mag. Acc.+4 "Mag.Atk.Bns."+4' },
+        },
       },
       ['Reprisal'] = 'Flash',
       ['Phalanx'] = {
@@ -78,28 +71,24 @@ return {
   },
   ['Divine Magic'] = {
     default = {
-      ammo = 'Ginsen',
+      ammo = 'Staunch Tathlum',
       head = {
         name = 'Souveran Schaller',
         augments = { 'HP+80', 'Enmity+7', 'Potency of "Cure" effect received +10%' },
       },
-      body = { name = 'Souveran Cuirass', augments = { 'HP+80', 'Enmity+7', 'Potency of "Cure" effect received +10%' } },
+      body = 'Chev. Cuirass +2',
       hands = {
         name = 'Souv. Handsch. +1',
         augments = { 'HP+105', 'Enmity+9', 'Potency of "Cure" effect received +15%' },
       },
-      legs = {
-        name = 'Souv. Diechlings +1',
-        augments = {
-          'HP+105',
-          'Enmity+9',
-          'Potency of "Cure" effect received +15%',
-        },
+      legs = { name = 'Carmine Cuisses +1', augments = { 'Accuracy+20', 'Attack+12', '"Dual Wield"+6' } },
+      feet = {
+        name = 'Odyssean Greaves',
+        augments = { 'Blood Pact Dmg.+1', '"Fast Cast"+3', 'Phalanx +4', 'Mag. Acc.+4 "Mag.Atk.Bns."+4' },
       },
-      feet = 'Souveran Schuhs',
       neck = 'Unmoving Collar',
-      waist = 'Creed Baudrier',
-      left_ear = 'Friomisi Earring',
+      waist = 'Rumination Sash',
+      left_ear = 'Halasz Earring',
       right_ear = 'Cryptic Earring',
       left_ring = 'Apeile Ring',
       right_ring = 'Supershear Ring',
@@ -109,49 +98,29 @@ return {
   },
   ['Blue Magic'] = {
     default = {
-      ammo = 'Ginsen',
+      ammo = 'Staunch Tathlum',
       head = {
         name = 'Souveran Schaller',
         augments = { 'HP+80', 'Enmity+7', 'Potency of "Cure" effect received +10%' },
       },
-      body = { name = 'Souveran Cuirass', augments = { 'HP+80', 'Enmity+7', 'Potency of "Cure" effect received +10%' } },
+      body = 'Chev. Cuirass +2',
       hands = {
         name = 'Souv. Handsch. +1',
         augments = { 'HP+105', 'Enmity+9', 'Potency of "Cure" effect received +15%' },
       },
-      legs = {
-        name = 'Souv. Diechlings +1',
-        augments = {
-          'HP+105',
-          'Enmity+9',
-          'Potency of "Cure" effect received +15%',
-        },
+      legs = { name = 'Carmine Cuisses +1', augments = { 'Accuracy+20', 'Attack+12', '"Dual Wield"+6' } },
+      feet = {
+        name = 'Odyssean Greaves',
+        augments = { 'Blood Pact Dmg.+1', '"Fast Cast"+3', 'Phalanx +4', 'Mag. Acc.+4 "Mag.Atk.Bns."+4' },
       },
-      feet = 'Souveran Schuhs',
       neck = 'Unmoving Collar',
-      waist = 'Creed Baudrier',
-      left_ear = 'Friomisi Earring',
+      waist = 'Rumination Sash',
+      left_ear = 'Halasz Earring',
       right_ear = 'Cryptic Earring',
       left_ring = 'Apeile Ring',
       right_ring = 'Supershear Ring',
       back = { name = "Rudianos's Mantle", augments = { 'HP+60', 'Eva.+20 /Mag. Eva.+20', 'Enmity+10' } },
     },
     spells = {},
-  },
-  ['SIRD'] = {
-    -- 15%
-    head = { name = 'Souveran Schaller', augments = { 'HP+80', 'Enmity+7', 'Potency of "Cure" effect received +10%' } },
-    -- 15%
-    body = 'Chev. Cuirass +2',
-    -- 20%
-    legs = { name = 'Carmine Cuisses +1', augments = { 'Accuracy+20', 'Attack+12', '"Dual Wield"+6' } },
-    -- 15%
-    feet = { name = 'Eschite Greaves', augments = { 'Mag. Evasion+15', 'Spell interruption rate down +15%', 'Enmity+7' } },
-    -- 10%
-    waist = 'Rumination Sash',
-    -- 5%
-    left_ear = 'Halasz Earring',
-    -- 10% merits
-    -- total 90%
   },
 }
